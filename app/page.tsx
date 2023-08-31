4a1e72c7876174457d86d19bf38db656ec455130
kris-liu-smile <kris.liu@bundleb2b.net>
2023-08-31 15:46:53 +0800

    fix: test2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Carousel } from 'components/carousel';
 import { ThreeItemGrid } from 'components/grid/three-items';
 import Footer from 'components/layout/footer';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 
 export const runtime = 'edge';
 
@@ -13,6 +13,22 @@ export const metadata = {
 };
 
 export default async function HomePage() {
+  useEffect(() => {
+    (window as any).B3 = { setting: { store_hash: 'imk8zz7jip' } }
+
+    const script1 = document.createElement('script');
+    
+    script1.src = 'https://cdn.bundleb2b.net/b2b/staging/storefront/index-legacy.8e12e9bd.js'
+    
+    document.body.append(script1)	
+
+    const script = document.createElement('script');
+    
+    script.src = 'https://cdn.bundleb2b.net/b2b/staging/storefront/polyfills-legacy.777236c5.js'
+    
+    document.body.append(script)	
+
+  }, [])
   return (
     <>
       <ThreeItemGrid />
